3e0288e8ffe87b97d0688012a0a27b56c9abff5a
sburns18 <sburns18@holynames-sea.org>
2018-07-24 04:09:36 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -8,4 +8,10 @@ puts "Great! Enter the time!"
 time = gets.strip 
 puts "Perfect! What is the host name?"
 host_name = gets.strip
-puts "Dear #{guest_name}, You are cordially invited to #{party_name} on "
+puts "Dear #{guest_name}, 
+
+You are cordially invited to #{party_name} on #{date} at #{time}. Please RSVP no later than October 30. 
+
+Sincerly, 
+
+#{host_name}"
